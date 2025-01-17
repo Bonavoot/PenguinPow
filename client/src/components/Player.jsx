b386c5aff0a801cb74b9c6a1cd8573939dd3eca4
Joseph <Jbonventre95@gmail.com>
2025-01-17 06:15:02 -0500

    added some preloading

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,17 +1,61 @@
+// Utility to preload assets
+const preloadAssets = (sources, type = "image") => {
+  sources.forEach((src) => {
+    if (type === "image") {
+      const img = new Image();
+      img.src = src;
+    } else if (type === "audio") {
+      const audio = new Audio();
+      audio.src = src;
+    }
+  });
+};
+
+// Refactored Player component
 import Fighter from "./Fighter";
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 import pumo from "../assets/pumo.png";
+import pumoWaddle from "../assets/pumo-waddle-old.gif";
+import crouching from "../assets/crouching.png";
+import daibaJumping from "../assets/daibaJumping.gif";
+import grabbing from "../assets/grabbing.png";
+import ready from "../assets/ready.png";
+import attack from "../assets/attack.png";
+import dodging from "../assets/dodging.gif";
+import throwing from "../assets/throwing.png";
+import hit from "../assets/lil-dinkey-avatar.png";
+
+import gameMusic from "../sounds/game-music.mp3";
+import grabSound from "../sounds/grab-sound.mp3";
+import attackSound from "../sounds/attack-sound.mp3";
+import hitSound from "../sounds/hit-sound.mp3";
+import dodgeSound from "../sounds/dodge-sound.mp3";
+
+// Preload assets at the start of the application
+const fighterImages = {
+  dinkey: pumo,
+  daiba: pumo,
+};
+const additionalImages = [
+  pumoWaddle,
+  crouching,
+  daibaJumping,
+  grabbing,
+  ready,
+  attack,
+  dodging,
+  throwing,
+  hit,
+];
+const audioSources = [gameMusic, grabSound, attackSound, hitSound, dodgeSound];
+
+preloadAssets(Object.values(fighterImages), "image");
+preloadAssets(additionalImages, "image");
+preloadAssets(audioSources, "audio");
 
 const Player = ({ index, fighter }) => {
-  const [penguin, setPenguin] = useState(pumo);
-
-  useEffect(() => {
-    if (fighter === "dinkey") {
-      setPenguin(pumo);
-    } else if (fighter === "daiba") {
-      setPenguin(pumo);
-    }
-  }, [fighter]);
+  // Determine the image source for the fighter
+  const penguin = useMemo(() => fighterImages[fighter] || pumo, [fighter]);
 
   return (
     <div className="player-lobby">
